ce7972326e512b6c9b0ee6959adbfe42107a18d0
Yaser Viqar <149509364+Yaser-Viqar@users.noreply.github.com>
2026-01-18 10:30:30 +0000

    commited done

--- projects/weather/weather.js
+++ projects/weather/weather.js
@@ -1,20 +1,30 @@
-const city_a=(document.querySelector('#cityInput')).value
+const city_a=(document.querySelector('#cityInput'))
 const search=document.querySelector('#searchBtn')
 search.addEventListener("click", () => {
     const city = city_a.value;
-const apirequest=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c7e9450c2ae6ad4cea07ac177fa99f3b&units=metric`
+    if(!city){
+        alert("enter location");
+
+}
+else{
+    const apirequest=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c7e9450c2ae6ad4cea07ac177fa99f3b&units=metric`
 
 const xhr= new XMLHttpRequest;
  xhr.open('GET',apirequest);
  xhr.onload=function(){
     if(xhr.status===200){
               const data = JSON.parse(xhr.responseText);
-      console.log(data);
+    //   console.log(data);
+      (document.querySelector('#temperature')).innerHTML=data.main.temp;
+      (document.querySelector('#humidity')).innerHTML=data.main.humidity;
+      (document.querySelector('#wind')).innerHTML=data.wind.speed;
     }
     else{
         console.log("not found")
     }
  
  }
+
     xhr.send();
+}
 })
